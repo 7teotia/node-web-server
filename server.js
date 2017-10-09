07cad190f2a4de445b51be751bd25a096d6873f2
Sandeep Teotia <sandeeteotia50@gmail.com>
2017-10-09 07:59:01 +0530

    Iinitial Commit

---
+++ server.js
@@ -0,0 +1,53 @@
+/*jshint esversion: 6 */
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/template/partials');
+hbs.registerHelper('getTime', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    var timstamp = new Date().toString();
+    fs.appendFile('server.log', timstamp + ' : ' + req.method + ' ' + req.path + '\n', (err) => {
+        if (err) console.log(`Unable to connect ${err}`);
+
+    });
+    // next();
+    if (req.path === '/about') {
+        res.render('maintence')
+    } else {
+        next();
+    }
+})
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/template');
+app.listen(3000, undefined, () => {
+    console.log(`Server  has been started at port 3000`);
+
+})
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Epress framwork !');
+    res.render('home.hbs', {
+        user: "sandeep",
+        like1: "Cricket",
+        like2: "Music",
+        time: new Date().getFullYear(),
+        pageTitle: "Home page"
+    })
+});
+
+app.get('/about', (req, res) => {
+    // res.send('This is all about learning node via Express id: ' + req.params.id);
+    res.render('about.hbs', {
+        pageTitle: "About Page",
+        time: new Date().getFullYear()
+    });
+});
